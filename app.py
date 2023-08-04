a3f517c17c2c7916c8cee0a2c29c2c27be15d492
Anas Nur Fauzan <anasnurf99@gmail.com>
2023-08-04 14:58:57 +0700

    Added ID number for each inputted value

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ from wtforms.validators import InputRequired
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your-secret-key"  # Replace with a secret key of your choice
 
-# Create a list to store the submitted numbers
-submitted_numbers = []
-
+# Create a list to store the submitted data as tuples (ID number, submitted number)
+submitted_data = []
 
 class NumberForm(FlaskForm):
     number_input = StringField("Number", validators=[InputRequired()])
@@ -18,11 +17,15 @@ class NumberForm(FlaskForm):
 @app.route("/", methods=["GET", "POST"])
 def numpad_input():
     form = NumberForm()
+
     if form.validate_on_submit():
         number = form.number_input.data
-        submitted_numbers.append(number)
+        id_number = request.form.get("id_number", "")
+        submitted_data.append((id_number, number))
         form.number_input.data = ""
-        print("Submitted numbers:", submitted_numbers)
+        print("Submitted data:")
+        for id_number, number in submitted_data:
+            print(f"ID: {id_number}, Number: {number}")
     return render_template("numpad_input.html", form=form)
 
 if __name__ == "__main__":
