5e6a113f9c5716b9d8232bd68c28322b06fa895b
Anas Nur Fauzan <anasnurf99@gmail.com>
2023-08-04 20:32:51 +0700

    IDK MAN WHY THIS IS REALLY HARD

--- app.py
+++ app.py
@@ -6,28 +6,38 @@ from wtforms.validators import InputRequired
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your-secret-key"  # Replace with a secret key of your choice
 
-# Create a list to store the submitted data as tuples (ID number, submitted number)
+# Create a dictionary to store submitted data (ID number as key, number as value)
 submitted_data = {}
+id_numbers = list(range(1, 85))
+current_id_index = 0  # Index of the current ID in the list
 
 class NumberForm(FlaskForm):
-    number_input = StringField("Number", validators=[InputRequired()])
+    number_input = StringField("Number")
     submit_button = SubmitField("Submit")
 
 
 @app.route("/", methods=["GET", "POST"])
 def numpad_input():
     form = NumberForm()
+    global current_id_index
 
     if form.validate_on_submit():
         number = form.number_input.data
-        id_number = request.form.get("id_number", "")
-        submitted_data[id_number] = number  # Store data in the dictionary
+        id_number = id_numbers[current_id_index]
+        submitted_data[id_number] = number
         form.number_input.data = ""
         print("Submitted data:")
         for id_number, number in submitted_data.items():
             print(f"ID: {id_number}, Number: {number}")
-        print(submitted_data)
-    return render_template("numpad_input.html", form=form)
+    
+    if request.method == "POST" and "change_id" in request.form:
+        direction = request.form["change_id"]
+        if direction == "previous" and current_id_index > 0:
+            current_id_index -= 1
+        elif direction == "next" and current_id_index < len(id_numbers) - 1:
+            current_id_index += 1
+
+    return render_template("numpad_input.html", form=form, current_id=id_numbers[current_id_index])
 
 if __name__ == "__main__":
     app.run(debug=True)
