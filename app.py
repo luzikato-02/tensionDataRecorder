715f3e76c205d7da6c79e18366f737bbfce0c2a8
Anas Nur Fauzan <anasnurf99@gmail.com>
2023-08-04 15:33:27 +0700

    ID number will show "1" when page loaded

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "your-secret-key"  # Replace with a secret key of your choice
 
 # Create a list to store the submitted data as tuples (ID number, submitted number)
-submitted_data = []
+submitted_data = {}
 
 class NumberForm(FlaskForm):
     number_input = StringField("Number", validators=[InputRequired()])
@@ -21,11 +21,12 @@ def numpad_input():
     if form.validate_on_submit():
         number = form.number_input.data
         id_number = request.form.get("id_number", "")
-        submitted_data.append((id_number, number))
+        submitted_data[id_number] = number  # Store data in the dictionary
         form.number_input.data = ""
         print("Submitted data:")
-        for id_number, number in submitted_data:
+        for id_number, number in submitted_data.items():
             print(f"ID: {id_number}, Number: {number}")
+        print(submitted_data)
     return render_template("numpad_input.html", form=form)
 
 if __name__ == "__main__":
