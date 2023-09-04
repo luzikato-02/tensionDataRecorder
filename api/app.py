4d951d8b0c8a7f0ded1400cde645b37a83244159
Anas Nur Fauzan <anasnurf99@gmail.com>
2023-09-04 20:24:59 +0700

    Modified requirements.txt

--- api/app.py
+++ api/app.py
@@ -176,4 +176,4 @@ if __name__ == '__main__':
     with app.app_context():  # Enter the application context
         # drop_tables()
         create_tables()
-    app.run(debug=True)
+    app.run(debug=False)
