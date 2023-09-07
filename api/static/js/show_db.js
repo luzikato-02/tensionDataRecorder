73060861cf53cc1f7efa5e8c2f8ab5220f8ed56c
Anas Nur Fauzan <anasnurf99@gmail.com>
2023-09-07 08:31:33 +0700

    Reduced columns

--- api/static/js/show_db.js
+++ api/static/js/show_db.js
@@ -5,8 +5,6 @@ $(document).ready(function () {
       dataSrc: "", // Data will be retrieved from this URL
     },
     columns: [
-        {data: 'id'},
-      { data: "datetime" },
       { data: "operator" },
       { data: "item_number" },
       { data: "machine_number" },
@@ -34,41 +32,6 @@ $(document).ready(function () {
       dataSrc: "", // Data will be retrieved from this URL
     },
     columns: [
-      {
-        data: "datetime",
-        render: function (data, type, row) {
-          if (type === "display" || type === "filter") {
-            // Define regular expressions to match both formats
-            var dateRegex = /(\d{1,2})\/(\d{1,2})\/(\d{4})/;
-            var timeRegex = /(\d{1,2})[.:](\d{1,2})[.:](\d{1,2})/;
-
-            // Try to match both date and time parts
-            var dateMatch = data.match(dateRegex);
-            var timeMatch = data.match(timeRegex);
-
-            if (dateMatch && dateMatch.length === 4) {
-              var year = parseInt(dateMatch[3]);
-              var month = parseInt(dateMatch[2]) - 1; // Months are 0-based
-              var day = parseInt(dateMatch[1]);
-
-              if (timeMatch && timeMatch.length === 4) {
-                var hour = parseInt(timeMatch[1]);
-                var minute = parseInt(timeMatch[2]);
-                var second = parseInt(timeMatch[3]);
-
-                // Create a new Date object with date and time
-                var date = new Date(year, month, day, hour, minute, second);
-                return date.toLocaleDateString();
-              } else {
-                // Create a new Date object with date only
-                var date = new Date(year, month, day);
-                return date.toLocaleDateString();
-              }
-            }
-          }
-          return data;
-        },
-      },
       { data: "operator" },
       { data: "machine_number" },
       { data: "production_order" },
