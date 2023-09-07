ffe4142e114c1a9c79ec73efc53b9ff2797164fb
Anas Nur Fauzan <anasnurf99@gmail.com>
2023-09-07 07:48:26 +0700

    Added entry date column and fixed its format

--- api/app.py
+++ api/app.py
@@ -3,10 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 import io
 import os
 
-username = os.environ.get('DB_USERNAME')
-password = os.environ.get('DB_PASSWORD')
-hostname = os.environ.get('DB_HOST')
-db_name = os.environ.get('DB_NAME')
+# username = os.environ.get('DB_USERNAME')
+# password = os.environ.get('DB_PASSWORD')
+# hostname = os.environ.get('DB_HOST')
+# db_name = os.environ.get('DB_NAME')
+
+username = 's46niqagxd5jjmw6ww0h'
+password = 'pscale_pw_FLLUpf2Uj83nOFlRCapGw7wnesJ40zSpnIWway5yWLe'
+hostname = 'aws.connect.psdb.cloud'
+db_name = 'tension_data'
+
 print(f"Database Host: {hostname}")
 port = 3306
 
@@ -194,4 +200,4 @@ if __name__ == '__main__':
     with app.app_context():  # Enter the application context
         # drop_tables()
         create_tables()
-    app.run(debug=True)
+    app.run(debug=False)
--- api/static/js/show_db.js
+++ api/static/js/show_db.js
@@ -1,59 +1,136 @@
-$(document).ready(function() {
-    $('#tw-data-table').DataTable({
-        ajax: {
-            url: '/get_tw_data',
-            dataSrc: ''  // Data will be retrieved from this URL
-        },
-        columns: [
-            { data: 'operator' },
-            { data: 'item_number'},
-            { data: 'machine_number'},
-            { data: 'rpm'},
-            { data: 'tpm'},
-            { data: 'stdTen'},
-            { data: 'devTen'},
-            {
-                data: null,
-                render: function(data, type, row) {
-                    return '<button class="tw-download-button" data-id="' + row.id + '">Download</button>';
-                }
-            }
-            // Add more column configurations
-        ]
-    });
+$(document).ready(function () {
+  $("#tw-data-table").DataTable({
+    ajax: {
+      url: "/get_tw_data",
+      dataSrc: "", // Data will be retrieved from this URL
+    },
+    columns: [
+      {
+        data: "datetime",
+        render: function (data, type, row) {
+          if (type === "display" || type === "filter") {
+            // Define regular expressions to match both formats
+            var dateRegex = /(\d{1,2})\/(\d{1,2})\/(\d{4})/;
+            var timeRegex = /(\d{1,2})[.:](\d{1,2})[.:](\d{1,2})/;
 
-    $('#wv-data-table').DataTable({
-        ajax: {
-            url: '/get_wv_data',
-            dataSrc: ''  // Data will be retrieved from this URL
-        },
-        columns: [
-            { data: 'operator' },
-            { data: 'machine_number' },
-            { data: 'production_order' },
-            { data: 'bale_num' },
-            { data: 'stdTen' },
-            { data: 'devTen' },
-            {
-                data: null,
-                render: function(data, type, row) {
-                    return '<button class="wv-download-button" data-id="' + row.id + '">Download</button>';
-                }
+            // Try to match both date and time parts
+            var dateMatch = data.match(dateRegex);
+            var timeMatch = data.match(timeRegex);
+
+            if (dateMatch && dateMatch.length === 4) {
+              var year = parseInt(dateMatch[3]);
+              var month = parseInt(dateMatch[2]) - 1; // Months are 0-based
+              var day = parseInt(dateMatch[1]);
+
+              if (timeMatch && timeMatch.length === 4) {
+                var hour = parseInt(timeMatch[1]);
+                var minute = parseInt(timeMatch[2]);
+                var second = parseInt(timeMatch[3]);
+
+                // Create a new Date object with date and time
+                var date = new Date(year, month, day, hour, minute, second);
+                return date.toLocaleDateString();
+              } else {
+                // Create a new Date object with date only
+                var date = new Date(year, month, day);
+                return date.toLocaleDateString();
+              }
             }
-            // Add more column configurations
-        ]
-    });
+          }
+          return data;
+        },
+      },
+      { data: "operator" },
+      { data: "item_number" },
+      { data: "machine_number" },
+      { data: "rpm" },
+      { data: "tpm" },
+      { data: "stdTen" },
+      { data: "devTen" },
+      {
+        data: null,
+        render: function (data, type, row) {
+          return (
+            '<button class="tw-download-button" data-id="' +
+            row.id +
+            '">Download</button>'
+          );
+        },
+      },
+      // Add more column configurations
+    ],
+  });
+
+  $("#wv-data-table").DataTable({
+    ajax: {
+      url: "/get_wv_data",
+      dataSrc: "", // Data will be retrieved from this URL
+    },
+    columns: [
+      {
+        data: "datetime",
+        render: function (data, type, row) {
+          if (type === "display" || type === "filter") {
+            // Define regular expressions to match both formats
+            var dateRegex = /(\d{1,2})\/(\d{1,2})\/(\d{4})/;
+            var timeRegex = /(\d{1,2})[.:](\d{1,2})[.:](\d{1,2})/;
+
+            // Try to match both date and time parts
+            var dateMatch = data.match(dateRegex);
+            var timeMatch = data.match(timeRegex);
+
+            if (dateMatch && dateMatch.length === 4) {
+              var year = parseInt(dateMatch[3]);
+              var month = parseInt(dateMatch[2]) - 1; // Months are 0-based
+              var day = parseInt(dateMatch[1]);
+
+              if (timeMatch && timeMatch.length === 4) {
+                var hour = parseInt(timeMatch[1]);
+                var minute = parseInt(timeMatch[2]);
+                var second = parseInt(timeMatch[3]);
+
+                // Create a new Date object with date and time
+                var date = new Date(year, month, day, hour, minute, second);
+                return date.toLocaleDateString();
+              } else {
+                // Create a new Date object with date only
+                var date = new Date(year, month, day);
+                return date.toLocaleDateString();
+              }
+            }
+          }
+          return data;
+        },
+      },
+      { data: "operator" },
+      { data: "machine_number" },
+      { data: "production_order" },
+      { data: "bale_num" },
+      { data: "stdTen" },
+      { data: "devTen" },
+      {
+        data: null,
+        render: function (data, type, row) {
+          return (
+            '<button class="wv-download-button" data-id="' +
+            row.id +
+            '">Download</button>'
+          );
+        },
+      },
+      // Add more column configurations
+    ],
+  });
 });
 
-
-$(document).on('click', '.tw-download-button', function() {
-    var entryId = $(this).data('id');
-    // Make an AJAX request to your Flask endpoint to initiate the download
-    window.location.href = '/download_tw/' + entryId;
+$(document).on("click", ".tw-download-button", function () {
+  var entryId = $(this).data("id");
+  // Make an AJAX request to your Flask endpoint to initiate the download
+  window.location.href = "/download_tw/" + entryId;
 });
 
-$(document).on('click', '.wv-download-button', function() {
-    var entryId = $(this).data('id');
-    // Make an AJAX request to your Flask endpoint to initiate the download
-    window.location.href = '/download_wv/' + entryId;
+$(document).on("click", ".wv-download-button", function () {
+  var entryId = $(this).data("id");
+  // Make an AJAX request to your Flask endpoint to initiate the download
+  window.location.href = "/download_wv/" + entryId;
 });
