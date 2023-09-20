c72fce7862f8f2c4b708f852b95b2d0842941ef5
Anas Nur Fauzan <anasnurf99@gmail.com>
2023-09-20 11:28:42 +0700

    Changed blob value to encodedURI

--- api/static/js/weaving_func.js
+++ api/static/js/weaving_func.js
@@ -594,7 +594,7 @@ function writeCSV(
   link.setAttribute("download", filename);
   document.body.appendChild(link);
 
-  var blob = new Blob([csvContent], { type: "text/csv" });
+  var blob = new Blob([encodedUri], { type: "text/csv" });
   // Create a FormData object to send both the CSV data
   let formData = new FormData(document.querySelector('form'));
   formData.append("datetime", currentDateTime);
