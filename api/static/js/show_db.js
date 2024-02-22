e6b202ab626f2b634988e707c1837c2b0cf4656f
Anas Nur Fauzan <anasnurf99@gmail.com>
2024-02-22 12:42:26 +0700

    Changed table framework to DataTable

--- api/static/js/show_db.js
+++ api/static/js/show_db.js
@@ -1,37 +1,68 @@
-function operateFormatter(value, row) {
-  return '<button class="btn btn-primary tw-download-button" data-id="' + row.id + '">Download</button>';
-}
-
-var $table = $('#tw-data-table');
-
-function queryParams(params) {
-  return {
-      // Additional parameters if needed
-      limit: params.limit,
-      offset: params.offset,
-      order: params.order,
-      sort: params.sort
-  };
-}
-
-var operateEvents = {
-  'click .tw-download-button': function (e, value, row, index) {
-      // Make an AJAX request to your Flask endpoint to initiate the download
-      window.location.href = "/download_tw/" + row.id;
-  }
-};
-
 $(document).ready(function () {
-  $table.bootstrapTable({
+  $("#tw-data-table").DataTable({
+    ajax: {
       url: "/get_tw_data",
-      columns: [
-          // ... Your column definitions
-      ],
-      queryParams: queryParams,
-      responseHandler: responseHandler,
-      // ... Other Bootstrap Table options
-      onClickRow: function (row, $element) {
-          // Handle row click event if needed
-      }
+      dataSrc: "", // Data will be retrieved from this URL
+    },
+    columns: [
+      { data: "id" },
+      { data: "operator" },
+      { data: "item_number" },
+      { data: "machine_number" },
+      { data: "rpm" },
+      { data: "tpm" },
+      { data: "stdTen" },
+      { data: "devTen" },
+      {
+        data: null,
+        render: function (data, type, row) {
+          return (
+            '<button class="tw-download-button" data-id="' +
+            row.id +
+            '">Download</button>'
+          );
+        },
+      },
+      // Add more column configurations
+    ],
   });
+
+  $("#wv-data-table").DataTable({
+    ajax: {
+      url: "/get_wv_data",
+      dataSrc: "", // Data will be retrieved from this URL
+    },
+    columns: [
+      { data: "id" },
+      { data: "operator" },
+      { data: "machine_number" },
+      { data: "production_order" },
+      { data: "bale_num" },
+      { data: "stdTen" },
+      { data: "devTen" },
+      {
+        data: null,
+        render: function (data, type, row) {
+          return (
+            '<button class="wv-download-button" data-id="' +
+            row.id +
+            '">Download</button>'
+          );
+        },
+      },
+      // Add more column configurations
+    ],
+  });
+});
+
+$(document).on("click", ".tw-download-button", function () {
+  var entryId = $(this).data("id");
+  // Make an AJAX request to your Flask endpoint to initiate the download
+  window.location.href = "/download_tw/" + entryId;
+});
+
+$(document).on("click", ".wv-download-button", function () {
+  var entryId = $(this).data("id");
+  // Make an AJAX request to your Flask endpoint to initiate the download
+  window.location.href = "/download_wv/" + entryId;
 });
